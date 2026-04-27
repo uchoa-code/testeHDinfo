a9516f9a2542dcfc2e02f800024df5691abb2b99
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-27 12:09:06 -0300

    Adicionando Filtro + Ordenação ao queryset

--- Tasks/views.py
+++ Tasks/views.py
@@ -4,5 +4,13 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all().order_by('-createdAt')
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        queryset = Task.objects.all().order_by('-createdAt')
+        completed = self.request.query_params.get('completed')
+
+        if completed is not None:
+            queryset = queryset.filter(completed=completed.lower() == 'true')
+        return queryset
+    
