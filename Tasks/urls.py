a3b93961a5e4b967d9e163832e98bbd156a03008
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-27 10:29:34 -0300

    Criação das Rotas da API

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
 
-urlpatterns = [
+router = DefaultRouter
+router.register(r'task', TaskViewSet)
 
-]
+urlpatterns = router.urls
