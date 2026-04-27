32b3118226decfe9b54f1157a20e937f2fb2b873
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-27 12:17:29 -0300

    bug fix

--- Tasks/views.py
+++ Tasks/views.py
@@ -4,6 +4,7 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
     def get_queryset(self):
