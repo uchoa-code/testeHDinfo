cf69e9e7c9c5466add1b388b90f87aecd010c3df
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-27 11:15:14 -0300

    Criação do método de validação de tamanho mínimo de título da Task

--- Tasks/serializers.py
+++ Tasks/serializers.py
@@ -5,3 +5,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def validate_title(self, value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError("Título muito curto")
+        return value
