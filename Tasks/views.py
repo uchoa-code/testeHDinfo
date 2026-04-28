4b6f24bab461c2ff95f3d03eb2d7386aee9bd7c0
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-28 17:27:19 -0300

    Criação do método "index" & adição do metodo "completed" com "@action"

--- Tasks/views.py
+++ Tasks/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+
+def index(request):
+    return render(request, 'tasks/index.html')
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
@@ -15,3 +22,11 @@ class TaskViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(completed=completed.lower() == 'true')
         return queryset
     
+    @action(detail=True, methods=['patch'])
+    def completed(self, request, pk=None):
+        task = self.get_object()
+        task.completed = request.data.get('completed', task.completed)
+        task.save()
+
+        serializer = self.get_serializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
