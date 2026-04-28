8f51b4d37edebb013a02667d00c527dec834bebf
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-28 17:24:22 -0300

    correção do atributo description & correção da identação do método "__str__"

--- Tasks/models.py
+++ Tasks/models.py
@@ -4,10 +4,10 @@ import uuid
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    description = models.CharField()
+    description = models.CharField(blank=True)
     completed = models.BooleanField(default=False)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
