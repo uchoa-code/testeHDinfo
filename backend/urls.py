e767b33349ac68f71bd1bd8b811bba7bb406b579
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-29 12:37:48 -0300

    renomeado pasta "task" para "temp_task"

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tasks.views import index
+from temp_tasks.views import index
 
 urlpatterns = [
     path('',index, name='home'),
