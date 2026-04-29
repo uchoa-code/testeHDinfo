b0adeac274f99e969ee1566e79671741a839401c
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-29 10:17:34 -0300

    fix import

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.decorators import action
