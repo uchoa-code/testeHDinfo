69d5ed735a800d8f19d5907c93fb67f59752b91c
uchoa-code <uchoajoaopedro@gmail.com>
2026-04-29 10:18:01 -0300

    fix description

--- Tasks/models.py
+++ Tasks/models.py
@@ -4,7 +4,7 @@ import uuid
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    description = models.CharField(blank=True)
+    description = models.CharField(max_length=600,blank=True)
     completed = models.BooleanField(default=False)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
